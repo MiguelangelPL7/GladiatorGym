87da2b457d45bfb4346aa69034c407ba4ad59c6a
Alejandro <aallel02@estudiantes.unileon.es>
2022-01-05 19:20:45 +0100

    Atributos de Material y Order

--- src/mvc/model/vo/Material.java
+++ src/mvc/model/vo/Material.java
@@ -1,4 +1,62 @@
 package mvc.model.vo;
 
 public class Material {
+
+    private String nameMaterial;
+    private float weightMaterial;
+    private int unitsMaterial;
+    //activity associated
+    private String othersMaterial;
+
+    ///GETTERS AND SETTERS///
+
+    ///NAME///
+    public String getNameMaterial()
+    {
+        return nameMaterial;
+    }
+
+    public void setNameMaterial(String nameMaterial)
+    {
+        this.nameMaterial = nameMaterial;
+    }
+
+    ///WEIGHT///
+    public float getWeightMaterial()
+    {
+        return weightMaterial;
+    }
+
+    public void setWeightMaterial(float weightMaterial)
+    {
+        this.weightMaterial = weightMaterial;
+    }
+
+    ///UNITS///
+    public int getUnitsMaterial()
+    {
+        return unitsMaterial;
+    }
+
+    public void setUnitsMaterial(int unitsMaterial)
+    {
+        this.unitsMaterial = unitsMaterial;
+    }
+
+    ///ACTIVITY ASSOCIATED///
+    //get
+
+    //set
+
+    ///OTHERS///
+    public String getOthersMaterial()
+    {
+        return othersMaterial;
+    }
+
+    public void setOthersMaterial(String othersMaterial)
+    {
+        this.othersMaterial = othersMaterial;
+    }
+
 }
--- src/mvc/model/vo/Order.java
+++ src/mvc/model/vo/Order.java
@@ -1,4 +1,87 @@
 package mvc.model.vo;
 
+import java.lang.reflect.Array;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Order {
+
+    private String nidOrder;
+    private String nameProviderOrder;
+    private float priceOrder;
+    private String orderDateOrder;
+    private String dateDeliveryOrder;
+    private float weightOrder;
+    //private Set<Material> materialsIncludedOrder = new HashSet<Material>();
+
+    ///GETTERS AND SETTERS///
+
+    ///NID///
+    public String getNidOrder()
+    {
+        return nidOrder;
+    }
+
+    public void setNidOrder(String nidOrder)
+    {
+        this.nidOrder = nidOrder;
+    }
+
+    ///NAME PROVIDER///
+    public String getNameProviderOrder()
+    {
+        return nameProviderOrder;
+    }
+
+    public void setNameProviderOrder(String nameProviderOrder)
+    {
+        this.nameProviderOrder = nameProviderOrder;
+    }
+
+    ///PRICE///
+    public float getPriceOrder()
+    {
+        return priceOrder;
+    }
+
+    public void setPriceOrder(float priceOrder)
+    {
+        this.priceOrder = priceOrder;
+    }
+
+    ///DATE OF THE ORDER///
+    public String getOrderDateOrder()
+    {
+        return orderDateOrder;
+    }
+
+    public void setOrderDateOrder(String orderDateOrder)
+    {
+        this.orderDateOrder = orderDateOrder;
+    }
+
+    ///DATE OF THE DELIVERY///
+    public String getDateDeliveryOrder()
+    {
+        return dateDeliveryOrder;
+    }
+
+    public void setDateDeliveryOrder(String dateDeliveryOrder)
+    {
+        this.dateDeliveryOrder = dateDeliveryOrder;
+    }
+
+    ///WEIGHT///
+    public float getWeightOrder()
+    {
+        return weightOrder;
+    }
+
+    public void setWeightOrder(float weightOrder)
+    {
+        this.weightOrder = weightOrder;
+    }
+
+    ///MATERIALS INCLUDED///
+
 }
