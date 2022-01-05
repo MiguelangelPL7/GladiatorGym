b103e8bb76c635c04c9983faf376004b0064c0d6
Alejandro <aallel02@estudiantes.unileon.es>
2022-01-05 13:15:31 +0100

    Atributos de member

--- src/mvc/model/vo/Member.java
+++ src/mvc/model/vo/Member.java
@@ -1,4 +1,177 @@
 package mvc.model.vo;
 
 public class Member {
+
+    private String dniMember;
+    private String nameMember;
+    private String surnamesMember;
+    private String rateMember;
+    private String dateSubscriptionMember;
+    private int priceSubscriptionMember;
+    private byte ageMember;
+    private String dateOfBirthdayMember;
+    private String paymentMethodMember;
+    private int phoneMember;
+    private String mailMember;
+    private String addressMember;
+    private boolean activeMember;
+    private int idMember;
+
+    ///GETTERS AND SETTERS///
+
+    ///DNI///
+    public String getDniMember()
+    {
+        return dniMember;
+    }
+
+    public void setDniMember(String dniMemberdniEmployee)
+    {
+        this.dniMember = dniMember;
+    }
+
+    ///NAME///
+    public String getNameMember()
+    {
+        return nameMember;
+    }
+
+    public void setNameMember(String nameMember)
+    {
+        this.nameMember = nameMember;
+    }
+
+    ///SURNAME///
+    public String getSurnamesMember()
+    {
+        return surnamesMember;
+    }
+
+    public void setSurnamesMember(String surnamesMember)
+    {
+        this.surnamesMember = surnamesMember;
+    }
+
+    ///RATE///
+    public String getRateMember()
+    {
+        return rateMember;
+    }
+
+    public void setRateMember(String rateMember)
+    {
+        this.rateMember = rateMember;
+    }
+
+    ///DATE OF SUBSCRIPTION///
+    public String getDateSubscriptionMember()
+    {
+        return dateSubscriptionMember;
+    }
+
+    public void setDateSubscriptionMember(String dateSubscriptionMember)
+    {
+        this.dateSubscriptionMember =  dateSubscriptionMember;
+    }
+
+    ///PRICE OF SUBSCRIPTION///
+    public int getPriceSubscriptionMember()
+    {
+        return priceSubscriptionMember;
+    }
+
+    public void setPriceSubscriptionMember(int  priceSubscriptionMember)
+    {
+        this.priceSubscriptionMember = priceSubscriptionMember;
+    }
+
+    ///AGE///
+    public byte getAgeMember()
+    {
+        return ageMember;
+    }
+
+    public void setAgeMember(byte ageMember)
+    {
+        this. ageMember = ageMember;
+    }
+
+
+    ///DATE OF BIRTHDAY///
+    public String getDateOfBirthdayMember()
+    {
+        return dateOfBirthdayMember;
+    }
+
+    public void setDateOfBirthdayMember(String dateOfBirthdayMember)
+    {
+        this.dateOfBirthdayMember = dateOfBirthdayMember;
+    }
+
+    ///PAYMENT METHOD///
+    public String getPaymentMethodMember()
+    {
+        return paymentMethodMember;
+    }
+
+    public void setPaymentMethodMember(String paymentMethodMember)
+    {
+        this.paymentMethodMember = paymentMethodMember;
+    }
+
+    ///PHONE NUMBER///
+    public int getPhoneMember()
+    {
+        return phoneMember;
+    }
+
+    public void setPhoneMember(int phoneMember)
+    {
+        this.phoneMember = phoneMember;
+    }
+
+    ///MAIL///
+    public String getMailMember()
+    {
+        return mailMember;
+    }
+
+    public void setMailMember(String mailMember)
+    {
+        this.mailMember = mailMember;
+    }
+
+    ///ADDRESS///
+    public String getAddressMember()
+    {
+        return addressMember;
+    }
+
+    public void setAddressMember(String  addressMember)
+    {
+        this.addressMember = addressMember;
+    }
+
+    ///ACTIVE: TRUE OR FALSE///
+    public boolean getActiveMember()
+   {
+       return activeMember;
+   }
+
+    public void setActiveMember(boolean  activeMember)
+    {
+        this.activeMember = activeMember;
+    }
+
+    ///ID///
+    public int getIdMember()
+    {
+        return idMember;
+    }
+
+    public void setIdMember(int  idMember)
+    {
+        this.idMember = idMember;
+    }
+
 }
